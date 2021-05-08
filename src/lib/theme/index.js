da621bea468db976963a932d6e1b758e4df1e521
2rv <>
2021-05-08 17:36:56 +0300

    upd theme file structure

--- src/lib/theme/index.js
+++ src/lib/theme/index.js
@@ -1,12 +1,3 @@
-import { keyframes } from 'styled-components';
-
 export { THEME_COLOR } from './theme.color';
 export { THEME_SIZE } from './theme.size';
-
-export const spacing = (num) => `${4 * Number(num)}px`;
-
-export const fade = keyframes`
-  from {
-    opacity: 0;
-  }
-`;
+export { spacing } from './theme.service';
---
+++ src/lib/theme/theme.service.js
@@ -0,0 +1,7 @@
+export const spacing = (num) => `${4 * Number(num)}px`;
+
+export const fade = keyframes`
+  from {
+    opacity: 0;
+  }
+`;
