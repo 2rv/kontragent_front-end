1b5ae88f9690b35b40b7af8a0fb70b5ce69c9e02
David Harutyunyan <54120867+DavidHarutyunyan-Harutyunarac@users.noreply.github.com>
2021-05-30 16:33:50 +0300

    layout elements typescript and few fixes

--- src/lib/elements/layout/content.layout.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { THEME_SIZE } from '../../theme';
-
-export const ContentLayout = styled.div`
-  width: ${(p) => THEME_SIZE.LAYOUT[p.type] || THEME_SIZE.LAYOUT.DEFAULT};
-  margin: 0 auto;
-`;
--- src/lib/elements/layout/indent.layout.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { THEME_SIZE } from '../../theme';
-
-export const IndentLayout = styled.div`
-  padding: ${(p) => THEME_SIZE.INDENT[p.type] || THEME_SIZE.INDENT.DEFAULT};
-  width: 100%;
-`;
