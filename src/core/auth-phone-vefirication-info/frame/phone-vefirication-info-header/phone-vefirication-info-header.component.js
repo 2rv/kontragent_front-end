31ed23e471e6fbf3de89c5a0f1503123d2028800
David Harutyunyan <54120867+DavidHarutyunyan-Harutyunarac@users.noreply.github.com>
2021-06-02 13:02:19 +0300

    updated phone-vefirication-info-header.component.js

--- src/core/auth-phone-vefirication-info/frame/phone-vefirication-info-header/phone-vefirication-info-header.component.js
+++ src/core/auth-phone-vefirication-info/frame/phone-vefirication-info-header/phone-vefirication-info-header.component.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-import styled from 'styled-components';
-
 import { PrimaryTitleText } from '../../../../lib/elements/text';
 
 export function PhoneVerificationInfoHeaderComponent() {
-  return <SignupFormHeader tid="AUTH_PHONE_VERIFICATION_INFO.HEADER" />;
+  return <PrimaryTitleText tid="AUTH_PHONE_VERIFICATION_INFO.HEADER" />;
 }
-
-const SignupFormHeader = styled(PrimaryTitleText)``;
