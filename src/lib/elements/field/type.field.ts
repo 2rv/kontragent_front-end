74549c7f1dc845f4c2212dc061bb1b57443e2166
David Harutyunyan <54120867+DavidHarutyunyan-Harutyunarac@users.noreply.github.com>
2021-06-09 16:49:34 +0300

    created select.field element

--- src/lib/elements/field/type.field.ts
+++ src/lib/elements/field/type.field.ts
@@ -1,10 +1,19 @@
 export interface FieldPrimaryPropsType {
-  titleTid: string,
-  placeholderTid: string,
-  name: string,
-  onChange: any,
-  onBlur: any,
-  value: any,
-  error: string,
-  type: string,
+  titleTid: string;
+  placeholderTid: string;
+  name: string;
+  onChange: any;
+  onBlur: any;
+  value: any;
+  error: string;
+  type: string;
+}
+
+type optionObject = { id: number; tid: string; tvalue?: object };
+export interface SelectFieldPropsType {
+  titleTid: string;
+  option: optionObject[];
+  onChange: any;
+  onBlur: any;
+  name: string;
 }
