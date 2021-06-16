ff696ecd6e3143aa34deadb655e5cede0cb1c166
David Harutyunyan <54120867+DavidHarutyunyan-Harutyunarac@users.noreply.github.com>
2021-06-16 13:14:01 +0300

    created auth-verification-email-confirm module

--- src/main/router/index.js
+++ src/main/router/index.js
@@ -8,12 +8,18 @@ import { LOGIN_ROUTE_PATH } from '../../core/login';
 import { SIGNUP_ROUTE_PATH } from '../../core/signup';
 import { AUTH_RECOVERY_ACCOUNT_ROUTE_PATH } from '../../core/auth-recovery-account';
 import { AUTH_CHANGE_PASSWORD_ROUTE_PATH } from '../../core/auth-change-password';
+import {
+  AUTH_VERIFICATION_EMAIL_CONFIRM_ROUTE_PATH,
+  AuthVerificationEmailConfirmRouter,
+} from '../../core/auth-verification-email-confirm';
 
 export const routes = {
   [LOGIN_ROUTE_PATH]: LoginRouter,
   [SIGNUP_ROUTE_PATH]: SignupRouter,
   [AUTH_RECOVERY_ACCOUNT_ROUTE_PATH]: AuthRecoveryAccountRouter,
   [AUTH_CHANGE_PASSWORD_ROUTE_PATH]: AuthChangePasswordRouter,
+  [AUTH_VERIFICATION_EMAIL_CONFIRM_ROUTE_PATH]:
+    AuthVerificationEmailConfirmRouter,
 };
 
 export const Router = routesInit(routes);
--- src/main/store/index.js
+++ src/main/store/index.js
@@ -16,6 +16,10 @@ import {
   authRecoveryAccountStore,
   AUTH_RECOVERY_ACCOUNT_STORE_NAME,
 } from '../../core/auth-recovery-account';
+import {
+  authVerificationEmailConfirmStore,
+  AUTH_VERIFICATION_EMAIL_CONFIRM_STORE_NAME,
+} from '../../core/auth-verification-email-confirm';
 
 export const reducers = combineReducers({
   [AUTH_STORE_NAME]: authStore,
@@ -25,6 +29,8 @@ export const reducers = combineReducers({
   [LOGIN_STORE_NAME]: loginStore,
   [AUTH_CHANGE_PASSWORD_STORE_NAME]: authChangePasswordStore,
   [AUTH_RECOVERY_ACCOUNT_STORE_NAME]: authRecoveryAccountStore,
+  [AUTH_VERIFICATION_EMAIL_CONFIRM_STORE_NAME]:
+    authVerificationEmailConfirmStore,
 });
 
 export { initStore } from './store.core';
