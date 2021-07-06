7422c361b789a7c10d701a1819501e2bf8b55046
Pasha1neo <pzx5@bk.ru>
2021-07-06 17:21:38 +0300

    fix-window-is-not-defined

--- src/core/dashboard/dashboard.page.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-import { DashboardLayout } from '../../lib/common/dashboard';
-export function DashboardPage() {
-  return <DashboardLayout></DashboardLayout>;
-}
--- src/pages/dashboard/index.js
+++
@@ -1,2 +0,0 @@
-import { DashboardPage } from '../../core/dashboard/dashboard.page';
-export default DashboardPage;
