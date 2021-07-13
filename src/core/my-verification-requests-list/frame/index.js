3d829a6b825fda5014af7a8f24504a92d6980639
TAbrahamyan <t.abrahamyann@gmail.com>
2021-07-13 20:37:52 +0400

    added my verifications requests list header filter

--- src/core/my-verification-requests-list/frame/index.js
+++ src/core/my-verification-requests-list/frame/index.js
@@ -1,1 +1,2 @@
 export { MyVerificationListContainer } from './my-verification-list';
+export { MyVerificationRequestsListHeaderFilterComponent } from './my-verification-requests-list-header-filter';
