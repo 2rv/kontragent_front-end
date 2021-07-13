117a1ebbb270460c4b5d837b606374c41fd4ea2e
Pasha1neo <pzx5@bk.ru>
2021-07-13 21:40:57 +0300

    sidebar-links

--- src/core/sidebar/sidebar.constant.js
+++ src/core/sidebar/sidebar.constant.js
@@ -1,22 +1,72 @@
 export const NAVMENU_ITEMS = [
   {
-    tid: 'SIDEBAR.NAV_MENU.NEWS_AND_NOTIFICATIONS',
-    pathname: '',
-    path: '/',
+    tid: 'Арбитраж',
+    pathname: '/arbitration',
+    path: '/arbitration',
   },
   {
-    tid: 'SIDEBAR.NAV_MENU.NEWS_AND_NOTIFICATIONS',
-    pathname: '/',
-    path: '/',
+    tid: 'Арбитражные дела',
+    pathname: '/arbitration-cases',
+    path: '/arbitration-cases',
   },
   {
-    tid: 'SIDEBAR.NAV_MENU.NEWS_AND_NOTIFICATIONS',
-    pathname: '/',
-    path: '/',
+    tid: 'Пополнение баланса',
+    pathname: '/balace-deposit',
+    path: '/balace-deposit',
   },
   {
-    tid: 'SIDEBAR.NAV_MENU.NEWS_AND_NOTIFICATIONS',
-    pathname: '/',
-    path: '/',
+    tid: 'История баланс',
+    pathname: '/balance-activity',
+    path: '/balance-activity',
+  },
+  {
+    tid: 'Участники компании',
+    pathname: '/company-members',
+    path: '/company-members',
+  },
+  {
+    tid: 'Панель управления',
+    pathname: '/dashboard',
+    path: '/dashboard',
+  },
+  {
+    tid: 'Отзывы контрагента',
+    pathname: '/kontragent-comments',
+    path: '/kontragent-comments',
+  },
+  {
+    tid: 'Мои запросы к юристам',
+    pathname: '/lawyer-request',
+    path: '/lawyer-request',
+  },
+  {
+    tid: 'Мои проверки',
+    pathname: '/my-verifications',
+    path: '/my-verifications',
+  },
+  {
+    tid: 'Информация о контрагенте',
+    pathname: '/kontragent-information',
+    path: '/kontragent-information',
+  },
+  {
+    tid: 'Страница моих рефералов',
+    pathname: '/referral',
+    path: '/referral',
+  },
+  {
+    tid: 'Создать запрос в поддержку',
+    pathname: '/support',
+    path: '/support',
+  },
+  {
+    tid: 'Восстановление пароля',
+    pathname: '/auth-recovery-account',
+    path: '/auth-recovery-account',
+  },
+  {
+    tid: 'Восстановление пароля',
+    pathname: '/login',
+    path: '/login',
   },
 ];
