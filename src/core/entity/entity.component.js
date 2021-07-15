37a6f47ab868849ebd04e6354d7a640026e5bf5a
TAbrahamyan <t.abrahamyann@gmail.com>
2021-07-15 18:33:38 +0400

    added primary loader

--- src/core/entity/entity.component.js
+++ src/core/entity/entity.component.js
@@ -3,11 +3,17 @@ import styled from 'styled-components';
 
 import { TabsComponent } from './frames/tabs';
 
+import { PrimaryLoader } from '../../lib/elements/loader';
 import { PrimaryTitleText } from '../../lib/elements/text';
 import { spacing, THEME_SIZE } from '../../lib/theme';
 
 export function EntityComponent(props) {
   const {
+    isPending,
+    isError,
+    isSuccess,
+    pageLoading,
+    errorMessage,
     tabsList,
     tabsComponentList,
     activeTabId,
@@ -15,15 +21,18 @@ export function EntityComponent(props) {
   } = props;
 
   return (
-    <Container>
-      <EntityText tid="Юрлицо" />
-      <TabsComponent tabsList={tabsList} activeTabId={activeTabId} activeTabHandler={activeTabHandler} />
-      {tabsComponentList.map((component) => {
-        if (component.id === activeTabId) {
-          return <React.Fragment key={component.id}>{component.component}</React.Fragment>;
-        }
-      })}
-    </Container>
+    <React.Fragment>
+      {(isPending || pageLoading) && <PrimaryLoader />}
+      <Container>
+        <EntityText tid="Юрлицо" />
+        <TabsComponent tabsList={tabsList} activeTabId={activeTabId} activeTabHandler={activeTabHandler} />
+        {tabsComponentList.map((component) => {
+          if (component.id === activeTabId) {
+            return <React.Fragment key={component.id}>{component.component}</React.Fragment>;
+          }
+        })}
+      </Container>
+    </React.Fragment>
   );
 }
 
