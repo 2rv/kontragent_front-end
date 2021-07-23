50331346ca523ecd5cdc5fdf2120768058598278
Pasha1neo <pzx5@bk.ru>
2021-07-24 00:21:03 +0300

    just

--- src/lib/elements/list/base-list.js
+++ src/lib/elements/list/base-list.js
@@ -14,7 +14,7 @@ export function BaseList(props) {
 
   return (
     <List>
-      {skeletonAction || true ? (
+      {skeletonAction ? (
         <ListSkeleton />
       ) : (
         listData.map((data, index) => (
