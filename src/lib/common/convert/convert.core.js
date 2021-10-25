2cddbbcaa0021417ce0bb616f2eba768c1351831
Mi4-04 <zakharovm2001@mail.ru>
2021-10-25 15:38:40 +0300

    convert

--- src/lib/common/convert/convert.core.js
+++ src/lib/common/convert/convert.core.js
@@ -1,6 +1,16 @@
 import { text } from '../text';
-import { revisionStatusType } from './convert.status';
+import {
+  revisionStatusType,
+  referalAchieventStatusType,
+} from './convert.status';
 
 export function convertRevisionStatus(status) {
   return { status: status, text: text(revisionStatusType[status]) };
+}
+
+export function converReferalAchievementStatus(status) {
+  return {
+    status: status,
+    text: text(referalAchieventStatusType[status]),
+  };
 }
