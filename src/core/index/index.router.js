b033ab856545c210be27da3655af5a9126e8b346
Taras Liush <taras.liush@gmail.com>
2021-10-29 14:03:30 +0300

    add const in indexrouter

--- src/core/index/index.router.js
+++ src/core/index/index.router.js
@@ -2,23 +2,21 @@ import {
   authRedirectLogged,
   authRedirectPrivated,
   authRedirectVerification,
-  authRedirectAdmin
+  authRedirectAdmin,
 } from '../../lib/common/auth';
-
+import { AUTH_SIGNUP_ROUTE_PATH } from '../../core/auth-signup/auth-signup.constant';
 import { AUTH_VERIFICATION_EMAIL_ROUTE_PATH } from '../../core/auth-verification-email';
 import { AUTH_VERIFICATION_PHONE_ROUTE_PATH } from '../../core/auth-verification-phone';
-import { USER_ADMIN_LIST_ROUTE_PATH } from '../../core/user-admin-list/user-admin-list.constant'
-
-
+import { USER_ADMIN_LIST_ROUTE_PATH } from '../../core/user-admin-list/user-admin-list.constant';
+import { COMPANY_ACCOUNT_LIST_ROUTE_PATH } from '../../core/company-account-list/company-account-list.constant';
 export function IndexRouter(ctx) {
-  authRedirectPrivated(ctx, '/auth/signup');
+  authRedirectPrivated(ctx, AUTH_SIGNUP_ROUTE_PATH);
 
   authRedirectVerification(
     ctx,
     AUTH_VERIFICATION_EMAIL_ROUTE_PATH,
     AUTH_VERIFICATION_PHONE_ROUTE_PATH,
   );
-  authRedirectAdmin(ctx,
-    USER_ADMIN_LIST_ROUTE_PATH)
-  authRedirectLogged(ctx, '/company');
+  authRedirectAdmin(ctx, USER_ADMIN_LIST_ROUTE_PATH);
+  authRedirectLogged(ctx, COMPANY_ACCOUNT_LIST_ROUTE_PATH);
 }
