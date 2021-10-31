7635acc3ae00c2d022f90155bcb9167e65702f04
David Harutyunyan <54120867+DavidHarutyunyan-Harutyunarac@users.noreply.github.com>
2021-10-31 16:09:27 +0300

    created router to redirect on unavailable company pages

--- src/core/company-account-item-balance/company-account-item-balance.router.js
+++ src/core/company-account-item-balance/company-account-item-balance.router.js
@@ -1,7 +1,11 @@
 import { setActivePath } from '../../lib/common/navigation/navigation.action';
-import { authRedirectPrivated } from '../../lib/common/auth';
+import {
+  authRedirectPrivated,
+  redirectUnavailableCompanyPage,
+} from '../../lib/common/auth';
 import { COMPANY_ACCOUNT_ITEM_BALANCE_ROUTE_PATH } from './company-account-item-balance.constant';
 export function CompanyAccountItemBalanceRouter(ctx) {
   ctx.store.dispatch(setActivePath(COMPANY_ACCOUNT_ITEM_BALANCE_ROUTE_PATH));
   authRedirectPrivated(ctx, '/');
+  redirectUnavailableCompanyPage(ctx, '/');
 }
