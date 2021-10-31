03bc8567bbbc6d3d5e33cb518b56608a04165524
Mi4-04 <zakharovm2001@mail.ru>
2021-10-31 16:53:37 +0300

    add redirect in pages company-account-item

--- src/core/company-account-item-member/company-account-item-member.router.js
+++ src/core/company-account-item-member/company-account-item-member.router.js
@@ -1,7 +1,11 @@
 import { setActivePath } from '../../lib/common/navigation/navigation.action';
-import { authRedirectPrivated } from '../../lib/common/auth';
+import {
+  authRedirectPrivated,
+  redirectUnavailableCompanyPage,
+} from '../../lib/common/auth';
 import { COMPANY_ACCOUNT_ITEM_MEMBER_ROUTE_PATH } from './company-account-item-member.constant';
 export function CompanyAccountItemMemberRouter(ctx) {
   ctx.store.dispatch(setActivePath(COMPANY_ACCOUNT_ITEM_MEMBER_ROUTE_PATH));
   authRedirectPrivated(ctx, '/');
+  redirectUnavailableCompanyPage(ctx, '/');
 }
--- src/core/company-account-item-revision-list/company-account-item-revision-list.router.js
+++ src/core/company-account-item-revision-list/company-account-item-revision-list.router.js
@@ -1,5 +1,8 @@
 import { setActivePath } from '../../lib/common/navigation/navigation.action';
-import { authRedirectPrivated } from '../../lib/common/auth';
+import {
+  authRedirectPrivated,
+  redirectUnavailableCompanyPage,
+} from '../../lib/common/auth';
 
 import { COMPANY_ACCOUNT_ITEM_REVISION_LIST_ROUTE_PATH } from './company-account-item-revision-list.constant';
 
@@ -8,4 +11,5 @@ export function CompanyAccountItemRevisionListRouter(ctx) {
     setActivePath(COMPANY_ACCOUNT_ITEM_REVISION_LIST_ROUTE_PATH),
   );
   authRedirectPrivated(ctx, '/');
+  redirectUnavailableCompanyPage(ctx, '/');
 }
