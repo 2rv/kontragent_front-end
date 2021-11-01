e6815de3e3b5dbe8755fa888cd58426970dddbef
taras_liush <taras.liush@gmail.com>
2021-11-01 10:01:27 +0300

    1st commit

--- src/main/next/app.core.js
+++ src/main/next/app.core.js
@@ -20,8 +20,13 @@ import { authSetData, authGetCookieToken } from '../../lib/common/auth';
 import { NavigationObserver } from '../../lib/common/navigation';
 
 import { setCurrentAuthCookie } from '../auth';
+import { authRedirectVerification } from '../../lib/common/auth/auth.redirect'
+import { AUTH_STORE_NAME } from "../../lib/common/auth/auth.constant"
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { AUTH_VERIFICATION_EMAIL_ROUTE_PATH } from '../../core/auth-verification-email'
+import { AUTH_VERIFICATION_PHONE_ROUTE_PATH } from '../../core/auth-verification-phone'
+import { redirect } from '../../main/navigation/navigation.core'
 
 class MyApp extends App {
   componentDidMount() {
@@ -51,12 +56,17 @@ class MyApp extends App {
       ? await Component.getInitialProps({ ...ctx, token })
       : { token };
 
+    if (token) {
+      if (ctx.pathname !== AUTH_VERIFICATION_EMAIL_ROUTE_PATH && ctx.pathname !== AUTH_VERIFICATION_PHONE_ROUTE_PATH && ctx.pathname !== '/auth/signup') {
+        authRedirectVerification(ctx, AUTH_VERIFICATION_EMAIL_ROUTE_PATH, AUTH_VERIFICATION_PHONE_ROUTE_PATH)
+      }
+    }
+    
     return { pageProps };
   }
 
   render() {
     const { Component, pageProps, store } = this.props;
-
     return (
       <ReduxProvider store={store}>
         <NavigationObserver />
