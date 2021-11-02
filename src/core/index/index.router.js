799701615ee4c636e844b533387f238832f44b25
taras_liush <taras.liush@gmail.com>
2021-11-02 12:45:11 +0300

    oops

--- src/core/index/index.router.js
+++ src/core/index/index.router.js
@@ -12,40 +12,6 @@ import { USER_ADMIN_LIST_ROUTE_PATH } from '../../core/user-admin-list/user-admi
 import { COMPANY_ACCOUNT_LIST_ROUTE_PATH } from '../../core/company-account-list/company-account-list.constant';
 export function IndexRouter(ctx) {
 
-  function codingScoreReportPercent(scores) {
-    let poorScore = 0;
-    let fairScore = 0;
-    let goodScore = 0;
-    let exellentScore = 0;
-    let eliteScore = 0;
-
-    let percentArr = [
-      `Poor: ${poorScore}`,
-      `Fair: ${fairScore}`,
-      `Good: ${goodScore}`,
-      `Exellent: ${exellentScore}`,
-      `Elite: ${eliteScore}`
-    ]
-
-    scores.map((userScore) => {
-      if (userScore <= 599) {
-        poorScore = poorScore + userScore
-      } else if (userScore >= 600 && userScore <= 699) {
-        fairScore = fairScore + userScore
-      } else if (userScore >= 700 && userScore <= 749) {
-        goodScore = goodScore + userScore
-      } else if (userScore >= 750 && userScore <= 799) {
-        exellentScore = exellentScore + userScore
-      } else if (userScore >= 800) {
-        eliteScore = eliteScore + userScore
-      }
-    })
-    console.log(percentArr)
-
-  }
-
-  codingScoreReportPercent([330, 723, 730, 825])
-
   authRedirectPrivated(ctx, AUTH_SIGNUP_ROUTE_PATH);
 
   authRedirectVerification(
