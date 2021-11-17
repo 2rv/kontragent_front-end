5f2864f76cd93186751dfa13e081e084b0601857
taras_liush <taras.liush@gmail.com>
2021-11-17 09:45:41 +0300

    Grid styles

--- src/core/company-account-item-revision-item/company-account-item-reivision-item.component.js
+++ src/core/company-account-item-revision-item/company-account-item-reivision-item.component.js
@@ -9,7 +9,7 @@ import Alert from '@mui/material/Alert';
 import { SkeletonListComponent } from '../../lib/common/skeleton/skeleton-list.component';
 
 import { text } from '../../lib/common/text';
-
+import Grid from '@mui/material/Grid';
 import { CompanyAccountItemRevisionItemPaymentContainer } from '../company-account-item-revision-item-payment/company-account-item-revision-item-payment.container';
 import { CompanyAccountItemRevisionItemListComponent } from './frames/company-account-item-reivision-item-list.component';
 import { CompanyAccountItemRevisionItemAccordionComponent } from './frames/company-account-item-revision-item-accordion.component';
@@ -24,62 +24,64 @@ export function CompanyAccountItemRevisionItemComponent({
 }) {
   return (
     <React.Fragment>
-      <Paper sx={{ p: 0 }}>
-        <Box>
-          <Typography
-            variant="title"
-            sx={{ px: 8, pt: 8, pb: 2 }}
-            component="div"
-          >
-            {data.title}
-          </Typography>
+      <Grid spacing={6} container>
+        <Grid item>
+          <Paper sx={{ p: 0 }}>
+            <Box>
+              <Typography
+                variant="title"
+                sx={{ px: 8, pt: 8, pb: 2 }}
+                component="div"
+              >
+                {data.title}
+              </Typography>
 
-          <ReviewStatus status={data.status} />
+              <ReviewStatus status={data.status} />
 
-          <Divider sx={{ mx: 8 }} />
+              <Divider sx={{ mx: 8 }} />
 
-          {isSuccess && (
-            <div>
-              <CompanyAccountItemRevisionItemListComponent data={data} />
-            </div>
-          )}
-          {isPending && (
-            <Box sx={{ pt: 4, px: 8, pb: 8 }}>
-              <SkeletonListComponent text={true} />
+              {isSuccess && (
+                <CompanyAccountItemRevisionItemListComponent data={data} />
+              )}
+              {isPending && (
+                <Box sx={{ pt: 4, px: 8, pb: 8 }}>
+                  <SkeletonListComponent text={true} />
+                </Box>
+              )}
+              {isError && (
+                <Box sx={{ pt: 4, px: 8, pb: 8 }}>
+                  <Alert severity="error">{text(`ERROR.${errorMessage}`)}</Alert>
+                </Box>
+              )}
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item>
+          <Paper sx={{ p: 0 }}>
+            <Box>
+              {isSuccess && (
+                <CompanyAccountItemRevisionItemAccordionComponent data={data} />
+              )}
+              {isPending && (
+                <Box sx={{ pt: 4, px: 8, pb: 8 }}>
+                  <SkeletonListComponent text={true} />
+                </Box>
+              )}
+              {isError && (
+                <Box sx={{ pt: 4, px: 8, pb: 8 }}>
+                  <Alert severity="error">{text(`ERROR.${errorMessage}`)}</Alert>
+                </Box>
+              )}
+            </Box>
+          </Paper>
+
+          {data.status === 4 && (
+            <Box sx={{ mt: 4 }}>
+              <CompanyAccountItemRevisionItemPaymentContainer data={data} />
             </Box>
           )}
-          {isError && (
-            <Box sx={{ pt: 4, px: 8, pb: 8 }}>
-              <Alert severity="error">{text(`ERROR.${errorMessage}`)}</Alert>
-            </Box>
-          )}
-        </Box>
-      </Paper>
-      <Paper>
-        <Box>
-          {isSuccess && (
-            <div>
-              <CompanyAccountItemRevisionItemAccordionComponent data={data} />
-            </div>
-          )}
-          {isPending && (
-            <Box sx={{ pt: 4, px: 8, pb: 8 }}>
-              <SkeletonListComponent text={true} />
-            </Box>
-          )}
-          {isError && (
-            <Box sx={{ pt: 4, px: 8, pb: 8 }}>
-              <Alert severity="error">{text(`ERROR.${errorMessage}`)}</Alert>
-            </Box>
-          )}
-        </Box>
-      </Paper>
-
-      {data.status === 4 && (
-        <Box sx={{ mt: 4 }}>
-          <CompanyAccountItemRevisionItemPaymentContainer data={data} />
-        </Box>
-      )}
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 }
--- src/core/company-account-item-revision-item/frames/company-account-item-revision-item-accordion.component.js
+++ src/core/company-account-item-revision-item/frames/company-account-item-revision-item-accordion.component.js
@@ -4,49 +4,52 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import { COMPANY_ACCOUNT_ITEM_REVISION_ITEM_DATA_NAME } from '../company-account-item-revision-item.constant';
 import { text } from '../../../lib/common/text';
 
 export const CompanyAccountItemRevisionItemAccordionComponent = ({ data }) => {
+  console.log(data[
+    COMPANY_ACCOUNT_ITEM_REVISION_ITEM_DATA_NAME.REVISION_COMPANIES
+  ])
   return (
-    <Grid>
-      {data[
-        COMPANY_ACCOUNT_ITEM_REVISION_ITEM_DATA_NAME.REVISION_COMPANIES
-      ].map((company) => (
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>{company.name}</Typography>
-            <Typography>{company.inn}</Typography>
-            <Typography>
-              {text(
-                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.ONE_PERIOD',
-              ) + company.firstPeriod}
-            </Typography>
-            <Typography>
-              {text(
-                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.TWO_PERIOD',
-              ) + company.secondPeriod}
-            </Typography>
-            <Typography>
-              {text(
-                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.THREE_PERIOD',
-              ) + company.thirdPeriod}
-            </Typography>
-            <Typography>
-              {text(
-                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.FOUR_PERIOD',
-              ) + company.thirdPeriod}
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>{company.description}</Typography>
-          </AccordionDetails>
-        </Accordion>
-      ))}
-    </Grid>
+    data[
+      COMPANY_ACCOUNT_ITEM_REVISION_ITEM_DATA_NAME.REVISION_COMPANIES
+    ].map((company) => (
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{company.name}</Typography>
+          <Typography>{company.inn}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            {text(
+              'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.ONE_PERIOD',
+            ) + company.firstPeriod}
+          </Typography>
+          <Typography>
+            {text(
+              'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.TWO_PERIOD',
+            ) + company.secondPeriod}
+          </Typography>
+          <Typography>
+            {text(
+              'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.THREE_PERIOD',
+            ) + company.thirdPeriod}
+          </Typography>
+          <Typography>
+            {text(
+              'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.FOUR_PERIOD',
+            ) + company.thirdPeriod}
+          </Typography>
+          <Typography>{company.description}</Typography>
+        </AccordionDetails>
+      </Accordion>
+    ))
   );
 };
