f7e34722bc144eb470aa11fa40d5b8db22b00644
taras_liush <taras.liush@gmail.com>
2021-11-17 15:45:16 +0300

    add revision-periods and fileDescriptin files to accordion

--- src/core/company-account-item-revision-item/frames/company-account-item-revision-item-accordion.component.js
+++ src/core/company-account-item-revision-item/frames/company-account-item-revision-item-accordion.component.js
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import { COMPANY_ACCOUNT_ITEM_REVISION_ITEM_DATA_NAME } from '../company-account-item-revision-item.constant';
 import { text } from '../../../lib/common/text';
 import Divider from '@mui/material/Divider';
+import { FileListElement } from '../../../lib/element/file-list.element';
 
 export const CompanyAccountItemRevisionItemAccordionComponent = ({ data }) => {
   console.log(data[
@@ -22,31 +23,49 @@ export const CompanyAccountItemRevisionItemAccordionComponent = ({ data }) => {
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography>{company.name}</Typography>
-            <Typography>{company.inn}</Typography>
+            <Typography>{company.name} {company.inn}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              {text(
-                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.ONE_PERIOD',
-              )}
-            </Typography>
-            <Typography>
-              {text(
-                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.TWO_PERIOD',
-              )}
-            </Typography>
-            <Typography>
-              {text(
-                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.THREE_PERIOD',
-              )}
-            </Typography>
-            <Typography>
-              {text(
-                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.FOUR_PERIOD',
-              )}
-            </Typography>
+            {company.year.map((year) => {
+              return (
+                <Typography>
+                  {year.year} ({year.firstPeriod ?
+                    text(
+                      'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.ONE_PERIOD',
+                    ) : null} {year.secondPeriod ?
+                      text(
+                        'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.TWO_PERIOD',
+                      ) : null} {year.thirdPeriod ?
+                        text(
+                          'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.THREE_PERIOD',
+                        ) : null} {year.fourthPeriod ?
+                          text(
+                            'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.FOUR_PERIOD',
+                          ) : null}
+                  )
+
+                </Typography>
+              )
+            })}
             <Typography>{company.description}</Typography>
+            <Typography gutterBottom variant="listTitle" component="div">
+              {text(
+                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.FILES_FOR_DESCRIBING_RESPONSE_RESULT',
+              )}
+            </Typography>
+            {company.fileDescription ? (
+              <FileListElement
+                list={
+                  company.fileDescription
+                }
+              />
+            ) : (
+              <Typography variant="body1">
+                {text(
+                  'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.NO_FILES_PROVIDED',
+                )}
+              </Typography>
+            )}
           </AccordionDetails>
         </Accordion>
         <Divider sx={{ mx: 8 }} />
