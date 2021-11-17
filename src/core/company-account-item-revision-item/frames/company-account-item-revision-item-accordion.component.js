6cfe93d08e48818d3f793c10f6846f3084536c19
taras_liush <taras.liush@gmail.com>
2021-11-17 09:59:33 +0300

    add divider

--- src/core/company-account-item-revision-item/frames/company-account-item-revision-item-accordion.component.js
+++ src/core/company-account-item-revision-item/frames/company-account-item-revision-item-accordion.component.js
@@ -3,10 +3,9 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import { COMPANY_ACCOUNT_ITEM_REVISION_ITEM_DATA_NAME } from '../company-account-item-revision-item.constant';
 import { text } from '../../../lib/common/text';
+import Divider from '@mui/material/Divider';
 
 export const CompanyAccountItemRevisionItemAccordionComponent = ({ data }) => {
   console.log(data[
@@ -16,40 +15,42 @@ export const CompanyAccountItemRevisionItemAccordionComponent = ({ data }) => {
     data[
       COMPANY_ACCOUNT_ITEM_REVISION_ITEM_DATA_NAME.REVISION_COMPANIES
     ].map((company) => (
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>{company.name}</Typography>
-          <Typography>{company.inn}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            {text(
-              'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.ONE_PERIOD',
-            ) + company.firstPeriod}
-          </Typography>
-          <Typography>
-            {text(
-              'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.TWO_PERIOD',
-            ) + company.secondPeriod}
-          </Typography>
-          <Typography>
-            {text(
-              'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.THREE_PERIOD',
-            ) + company.thirdPeriod}
-          </Typography>
-          <Typography>
-            {text(
-              'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.FOUR_PERIOD',
-            ) + company.thirdPeriod}
-          </Typography>
-          <Typography>{company.description}</Typography>
-        </AccordionDetails>
-      </Accordion>
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>{company.name}</Typography>
+            <Typography>{company.inn}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              {text(
+                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.ONE_PERIOD',
+              )}
+            </Typography>
+            <Typography>
+              {text(
+                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.TWO_PERIOD',
+              )}
+            </Typography>
+            <Typography>
+              {text(
+                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.THREE_PERIOD',
+              )}
+            </Typography>
+            <Typography>
+              {text(
+                'COMPANY_ACCOUNT_ITEM_REVISION.REVISION_ITEM.PERIODS.FOUR_PERIOD',
+              )}
+            </Typography>
+            <Typography>{company.description}</Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Divider sx={{ mx: 8 }} />
+      </div>
     ))
   );
 };
