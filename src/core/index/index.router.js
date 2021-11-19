5a3f08861ff2db4d9225dcbdd19c3de222187b5b
Mi4-04 <zakharovm2001@mail.ru>
2021-11-19 19:49:40 +0300

    add pages-error

--- src/core/index/index.router.js
+++ src/core/index/index.router.js
@@ -9,9 +9,11 @@ import { USER_ADMIN_LIST_ROUTE_PATH } from '../../core/user-admin-list/user-admi
 import { COMPANY_ACCOUNT_LIST_ROUTE_PATH } from '../../core/company-account-list/company-account-list.constant';
 export function IndexRouter(ctx) {
   authRedirectPrivated(ctx, AUTH_SIGNUP_ROUTE_PATH);
-
+  if (authRedirectPrivated(ctx, AUTH_SIGNUP_ROUTE_PATH)) return;
   authRedirectVerification(ctx);
-
+  if (authRedirectVerification(ctx)) return;
   authRedirectNotAdmin(ctx, COMPANY_ACCOUNT_LIST_ROUTE_PATH);
+  if (authRedirectNotAdmin(ctx, COMPANY_ACCOUNT_LIST_ROUTE_PATH)) return;
   authRedirectAdmin(ctx, USER_ADMIN_LIST_ROUTE_PATH);
+  if (authRedirectAdmin(ctx, USER_ADMIN_LIST_ROUTE_PATH)) return;
 }
