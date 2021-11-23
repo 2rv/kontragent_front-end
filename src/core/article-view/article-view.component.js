84d3023ae1fa4226e454528583b06e80b89e430c
taras_liush <taras.liush@gmail.com>
2021-11-23 09:30:18 +0300

    add right skeleton

--- src/core/article-view/article-view.component.js
+++ src/core/article-view/article-view.component.js
@@ -1,14 +1,15 @@
 import { Article } from './frames/article.component';
 import { ARTICLE_DATA_NAME } from './article-view.constant';
-
+import Skeleton from '@mui/material/Skeleton';
 import Link from '@mui/material/Link';
 import { redirect } from '../../main/navigation/navigation.core';
-
+import React from 'react';
 import { Grid } from '@material-ui/core';
 import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import { SkeletonListComponent } from '../../lib/common/skeleton/skeleton-list.component';
 
 export function ArticleViewComponent(props) {
   const { loadDataPending, loadDataSuccess, article, isadmin, articleId } =
@@ -35,9 +36,8 @@ export function ArticleViewComponent(props) {
                 redirect(AUTH_RECOVERY_ACCOUNT_ROUTE_PATH);
               }}
             >
-              {`${article[ARTICLE_DATA_NAME.FIRSTNAME]} ${
-                article[ARTICLE_DATA_NAME.LASTNAME]
-              }`}
+              {`${article[ARTICLE_DATA_NAME.FIRSTNAME]} ${article[ARTICLE_DATA_NAME.LASTNAME]
+                }`}
             </Link>
           </Box>
         )}
@@ -50,9 +50,14 @@ export function ArticleViewComponent(props) {
             />
           )}
           {loadDataPending && (
-            <Box sx={{ pt: 4, width: '100%' }}>
-              <LinearProgress />
-            </Box>
+            <Grid>
+              <Box sx={{ pt: 4, px: 2, pb: 2 }}>
+                <Skeleton variant="rectangular" sx={{ height: '120px' }} />
+              </Box>
+              <Box sx={{ pt: 4, px: 2, pb: 2 }}>
+                <Skeleton variant="rectangular" sx={{ height: '120px' }} />
+              </Box>
+            </Grid>
           )}
         </Grid>
       </Box>
