e29af235240c395a032dc6f8b29a3f37b2da7aa6
Mi4-04 <zakharovm2001@mail.ru>
2021-11-23 12:37:56 +0300

    add redirect in user-admin-item-info

--- src/core/article-view/article-view.component.js
+++ src/core/article-view/article-view.component.js
@@ -3,12 +3,11 @@ import { ARTICLE_DATA_NAME } from './article-view.constant';
 import Skeleton from '@mui/material/Skeleton';
 import Link from '@mui/material/Link';
 import { redirect } from '../../main/navigation/navigation.core';
-import { USER_ADMIN_ITEM_INFO_ROUTE_PATH_DYNAMIC } from '../user-admin-item-info';
-
 import { Grid } from '@material-ui/core';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import { USER_ADMIN_ITEM_INFO_ROUTE_PATH_DYNAMIC } from '../user-admin-item-info/user-admin-item-info.constant';
 
 export function ArticleViewComponent(props) {
   const { loadDataPending, loadDataSuccess, article, isadmin, articleId } =
@@ -28,23 +27,22 @@ export function ArticleViewComponent(props) {
             <Typography>
               {'Дата создания: ' + article[ARTICLE_DATA_NAME.CREATE_DATE]}
             </Typography>
-            {isadmin && (
-              <Link
-                component="button"
-                variant="body2"
-                onClick={() => {
-                  redirect(
-                    USER_ADMIN_ITEM_INFO_ROUTE_PATH_DYNAMIC(
-                      article[ARTICLE_DATA_NAME.ID],
-                    ),
-                  );
-                }}
-              >
-                {`автор: ${article[ARTICLE_DATA_NAME.FIRSTNAME]} ${
-                  article[ARTICLE_DATA_NAME.LASTNAME]
-                }`}
-              </Link>
-            )}
+            <Link
+              component="button"
+              variant="body2"
+              onClick={() => {
+                redirect(USER_ADMIN_ITEM_INFO_ROUTE_PATH_DYNAMIC, {
+                  dynamic: true,
+                  params: {
+                    userId: article[ARTICLE_DATA_NAME.ID],
+                  },
+                });
+              }}
+            >
+              {`${article[ARTICLE_DATA_NAME.FIRSTNAME]} ${
+                article[ARTICLE_DATA_NAME.LASTNAME]
+              }`}
+            </Link>
           </Box>
         )}
         <Grid>
