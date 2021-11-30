cd971ac44d31135f5c48fb4123289af5ec32ce69
David Harutyunyan <54120867+harutyunaraci@users.noreply.github.com>
2021-11-30 19:02:09 +0300

    comment authRedirectPrivated

--- src/core/company-account-list/company-account-list.router.js
+++ src/core/company-account-list/company-account-list.router.js
@@ -8,7 +8,7 @@ import { COMPANY_ACCOUNT_LIST_ROUTE_PATH } from './company-account-list.constant
 
 export function CompanyAccountListRouter(ctx) {
   ctx.store.dispatch(setActivePath(COMPANY_ACCOUNT_LIST_ROUTE_PATH));
-  if (authRedirectPrivated(ctx, '/')) return;
+  // if (authRedirectPrivated(ctx, '/')) return;
   if (authRedirectVerification(ctx)) return;
   if (authRedirectAdmin(ctx, '/')) return;
 }
