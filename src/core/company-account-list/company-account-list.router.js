eacf701899890d01cfa0314f1eded073ee26d4f5
David Harutyunyan <54120867+harutyunaraci@users.noreply.github.com>
2021-11-30 19:11:53 +0300

    çommented setActivePath

--- src/core/company-account-list/company-account-list.router.js
+++ src/core/company-account-list/company-account-list.router.js
@@ -7,7 +7,7 @@ import {
 import { COMPANY_ACCOUNT_LIST_ROUTE_PATH } from './company-account-list.constant';
 
 export function CompanyAccountListRouter(ctx) {
-  ctx.store.dispatch(setActivePath(COMPANY_ACCOUNT_LIST_ROUTE_PATH));
+  // ctx.store.dispatch(setActivePath(COMPANY_ACCOUNT_LIST_ROUTE_PATH));
   // if (authRedirectPrivated(ctx, '/')) return;
   // if (authRedirectVerification(ctx)) return;
   // if (authRedirectAdmin(ctx, '/')) return;
