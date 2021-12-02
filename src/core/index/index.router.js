fc52ffb2b27122cee3c738854244ec1b94c23b1c
David Harutyunyan <54120867+harutyunaraci@users.noreply.github.com>
2021-12-02 13:54:25 +0300

    edited index.router

--- src/core/index/index.router.js
+++ src/core/index/index.router.js
@@ -4,12 +4,12 @@ import {
   authRedirectNotAdmin,
   authRedirectAdmin,
 } from '../../lib/common/auth';
-import { AUTH_SIGNUP_ROUTE_PATH } from '../../core/auth-signup/auth-signup.constant';
-import { USER_ADMIN_LIST_ROUTE_PATH } from '../../core/user-admin-list/user-admin-list.constant';
-import { COMPANY_ACCOUNT_LIST_ROUTE_PATH } from '../../core/company-account-list/company-account-list.constant';
+import { AUTH_SIGNUP_ROUTE_PATH } from '../../core/auth-signup';
+import { USER_ADMIN_LIST_ROUTE_PATH } from '../../core/user-admin-list';
+import { COMPANY_ACCOUNT_LIST_ROUTE_PATH } from '../../core/company-account-list';
 export function IndexRouter(ctx) {
-  if (authRedirectPrivated(ctx, AUTH_SIGNUP_ROUTE_PATH)) return;
-  if (authRedirectVerification(ctx)) return;
-  if (authRedirectNotAdmin(ctx, COMPANY_ACCOUNT_LIST_ROUTE_PATH)) return;
-  if (authRedirectAdmin(ctx, USER_ADMIN_LIST_ROUTE_PATH)) return;
+  if (authRedirectPrivated(ctx, AUTH_SIGNUP_ROUTE_PATH)) return true;
+  if (authRedirectVerification(ctx)) return true;
+  if (authRedirectNotAdmin(ctx, COMPANY_ACCOUNT_LIST_ROUTE_PATH)) return true;
+  if (authRedirectAdmin(ctx, USER_ADMIN_LIST_ROUTE_PATH)) return true;
 }
