84cfbbfec7f8a6d1bef5f9de5518bc9390377032
pasha1neo <pasha1neo@mail.ru>
2022-02-18 15:16:23 +0300

    landing-footer-fix

--- src/core/landing-footer/landing-footer.container.js
+++ src/core/landing-footer/landing-footer.container.js
@@ -1,34 +1,11 @@
-import { useReducer, useEffect } from 'react';
 import styled from 'styled-components';
-import {
-  getRequestData,
-  getRequestErrorMessage,
-  isRequestError,
-  isRequestPending,
-  isRequestSuccess,
-} from '../../main/store/store.service';
 import { LandingLayout } from '../../lib/common/landing';
-
-import { initialState, LandingFooterReducer } from './landing-footer.reducer';
-import { getAnalyticsAction } from './landing-footer.action';
 import { LandingFooterComponent } from './landing-footer.component';
 
 export function LandingFooterContainer() {
-  const [state, setState] = useReducer(LandingFooterReducer, initialState);
-
-  useEffect(() => {
-    getAnalyticsAction()(setState);
-  }, []);
-
   return (
     <Layout>
-      <LandingFooterComponent
-        isPending={isRequestPending(state.request)}
-        isSuccess={isRequestSuccess(state.request)}
-        isError={isRequestError(state.request)}
-        errorMessage={getRequestErrorMessage(state.request)}
-        data={getRequestData(state.request)}
-      />
+      <LandingFooterComponent />
     </Layout>
   );
 }
